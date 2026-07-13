7955661c90eb4a26354247636c562ffd36543458
Smart Campus <smart-campus@users.noreply.github.com>
2026-07-13 12:17:15 +0800

    Preserve API query parameters on Vercel

--- api/index.js
+++ api/index.js
@@ -3,6 +3,10 @@ const handler = require("../server/index");
 module.exports = (req, res) => {
   const url = new URL(req.url, "http://localhost");
   const route = url.searchParams.get("route");
-  if (route) req.url = `/api/${route}`;
+  if (route) {
+    url.searchParams.delete("route");
+    const query = url.searchParams.toString();
+    req.url = `/api/${route}${query ? `?${query}` : ""}`;
+  }
   return handler(req, res);
 };
